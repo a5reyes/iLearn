3c354e610e3bd6ff416ce36497d2e2cdd5941964
whatshouldiuseforthisnow <sauravniroula77@gmail.com>
2025-10-21 16:23:38 -0400

    Update Classroom.java
    sadjkla

--- src/Classroom.java
+++ src/Classroom.java
@@ -1,3 +1,34 @@
 public class Classroom {
-    systmme.oprahushdn
+    int classroomId;
+	String teacher;
+	String discussions;
+	
+	public Classroom(int classroomId, String teacher, String discussions) {
+		this.classroomId = classroomId;
+		this.teacher = teacher;
+		this.discussions = discussions;
+	}
+	
+	
+
+	public int getClassroomId() {
+		return classroomId;
+	}
+	public void setClassroomId(int classroomId) {
+		this.classroomId = classroomId;
+	}
+	public String getTeacher() {
+		return teacher;
+	}
+	public void setTeacher(String teacher) {
+		this.teacher = teacher;
+	}
+	public String getDiscussions() {
+		return discussions;
+	}
+	public void setDiscussions(String discussions) {
+		this.discussions = discussions;
+	}
+	
+	
 }
