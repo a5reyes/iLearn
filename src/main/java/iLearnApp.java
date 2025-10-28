ba5d49456b436f04a366a21ae7d78a36ca47e43d
M1charette <mcharette010805@gmail.com>
2025-10-28 16:16:06 -0400

    FXML

--- src/main/java/iLearnApp.java
+++ src/main/java/iLearnApp.java
@@ -1,3 +1,27 @@
-public class iLearnApp {
-    
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class iLearnApp extends Application {
+    /**
+     * Creates the GUI page, set its height/width and gives it a name
+     * @param stage - the area in which the GUI is created
+     * @throws IOException watches to make sure no exception gives an error
+     */
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(iLearnApp.class.getResource("app.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 300, 500);
+        stage.setTitle("iLearn");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
