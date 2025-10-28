6168e4716d20e01e05a7b996616fc92f6554442c
a5reyes <A5REYES@student.bridgew.edu>
2025-10-28 15:51:08 -0400

    FXML

---
+++ src/main/java/FXMLExample.java
@@ -0,0 +1,10 @@
+@Override
+    public void start(Stage stage) throws Exception {
+       Parent root = FXMLLoader.load(getClass().getResource("fxml_example.fxml"));
+    
+        Scene scene = new Scene(root, 300, 275);
+    
+        stage.setTitle("FXML Welcome");
+        stage.setScene(scene);
+        stage.show();
+    }
