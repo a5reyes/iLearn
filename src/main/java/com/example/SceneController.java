a57daca7346abb8d0dbabb48eef8fc2514d1ad5e
M1charette <mcharette010805@gmail.com>
2025-10-30 15:54:21 -0400

    Back button
    added a back button to each page

--- src/main/java/com/example/SceneController.java
+++ src/main/java/com/example/SceneController.java
@@ -47,4 +47,12 @@ public class SceneController {
         stage.setScene(scene);
         stage.show();
     }
+
+    public void backToMain(ActionEvent event) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("MainPage.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
