baea352ddf0b64e689f50893bf7aa863dac51cba
a5reyes <A5REYES@student.bridgew.edu>
2025-11-03 14:09:59 -0500

    *

--- src/main/java/com/example/Assignments.java
+++ src/main/java/com/example/Assignments.java
@@ -6,8 +6,8 @@ import com.iLearn.Classroom;
 public class Assignments extends Classroom {
     private List<String> assignments;
      
-	public Assignments(List<String> assignments, int classroomId, String teacher, String discussions, String meetingTime) {
-        super(classroomId, teacher, discussions, meetingTime);
+	public Assignments(List<String> assignments, String classroomName, int classroomId, String teacher, String[] discussions, String meetingTime) {
+        super(classroomName, classroomId, teacher, discussions, meetingTime);
         this.assignments = assignments;
     }
 
